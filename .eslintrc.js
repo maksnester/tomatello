c6d3596ef9962a2652c28d78b22f71c465cd32d8
Nesterenko Maxim <alendorff@gmail.com>
2019-03-02 16:25:18 +0200

    chore(.eslintrc): do not prefer interfaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
     '@typescript-eslint/no-unused-vars': 'error'
   },
 }
